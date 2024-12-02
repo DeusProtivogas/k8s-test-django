da4fb773bdf8771a9b399c38197d65bffe5f2527
DeusProtivogas <kirill2689@gmail.com>
2024-12-02 22:25:53 +0300

    Add files via upload

--- backend_main_django/src/webapp/settings.py
+++ backend_main_django/src/webapp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['192.168.59.115', '127.0.0.1', 'localhost'])
 
 
 # Application definition
